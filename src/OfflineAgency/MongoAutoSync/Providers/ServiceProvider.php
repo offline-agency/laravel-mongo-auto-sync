7ba7fc5a6dc7c7e2350395b261a26d041d03a0cc
giacomo92 <giacomo.fabbian@gmail.com>
2019-01-24 12:55:50 +0100

    [GF] Added package to namespace provider

--- src/OfflineAgency/MongoAutoSync/Providers/ServiceProvider.php
+++ src/OfflineAgency/MongoAutoSync/Providers/ServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OfflineAgency\MongoAutoSync\Providers;
+namespace Package\OfflineAgency\MongoAutoSync\Providers;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
