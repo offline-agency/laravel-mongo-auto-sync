92dbe169c670712a67d156ffab58e4c1b7c2972a
Giacomo Fabbian <giacomo.fabbian@gmail.com>
2019-04-12 11:03:41 +0200

    [GF] Fix bson that return from raw random function

--- src/Http/Models/MDModel.php
+++ src/Http/Models/MDModel.php
@@ -2,50 +2,51 @@
 
 namespace OfflineAgency\MongoAutoSync\Http\Models;
 
+use DateTime;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 use OfflineAgency\MongoAutoSync\Traits\ModelAdditionalMethod;
 use OfflineAgency\MongoAutoSync\Traits\MongoSyncTrait;
 
 class MDModel extends Eloquent
 {
-    protected $connection = 'mongodb';
-    protected $dates = ['deleted_at', 'created_at', 'updated_at'];
-    use MongoSyncTrait, ModelAdditionalMethod;
+	protected $connection = 'mongodb';
+	protected $dates = ['deleted_at', 'created_at', 'updated_at'];
+	use MongoSyncTrait, ModelAdditionalMethod;
 
-    /**
-     * Overload model __construct.
-     *
-     *  (Optional)
-     */
+	/**
+	 * Overload model __construct.
+	 *
+	 *  (Optional)
+	 */
 
 
-    public function __construct(array $attributes = [])
-    {
+	public function __construct(array $attributes = [])
+	{
 
-        parent::__construct($attributes);
-    }
+		parent::__construct($attributes);
+	}
 
-    /**
-     * Overload model save.
-     *
-     * $name_equals string Assert User's name (Optional)
-     */
-    public function save(array $options = array())
-    {
+	/**
+	 * Overload model save.
+	 *
+	 * $name_equals string Assert User's name (Optional)
+	 */
+	public function save(array $options = array())
+	{
 
-        // Do great things...
+		// Do great things...
 
-        parent::save($options); // Calls Default Save
-    }
+		parent::save($options); // Calls Default Save
+	}
 
-    public function delete()
-    {
-        $options = array();
-        parent::save($options); // Calls Default Save
-        // Do great things...
+	public function delete()
+	{
+		$options = array();
+		parent::save($options); // Calls Default Save
+		// Do great things...
 
-        parent::delete(); // Calls Default Save
-    }
+		parent::delete(); // Calls Default Save
+	}
 
 	/**
 	 * @return mixed
@@ -69,7 +70,46 @@ class MDModel extends Eloquent
 	 * @return mixed
 	 */
 	public function getRandom($size = 3){
-		return parent::raw(function($collection) use ($size){ return $collection->aggregate([ ['$sample' => ['size' => $size]] ]); });
+		$now = new DateTime('now');
+		$tags = parent::raw(function($collection) use ($size){
+			return $collection->aggregate([ ['$sample' => ['size' => $size]] ]);
+		});
+		return $this->fixBson($tags);
 	}
 
+	/**
+	 * @param $item
+	 *
+	 * @return array
+	 */
+	function formatTypeBson($item) {
+
+		if (isset($item) && gettype($item) == "object" && (get_class($item) == "MongoDB\Model\BSONArray" || get_class($item) == "MongoDB\Model\BSONDocument")) {
+
+			return (array)$item->bsonSerialize();
+
+		} else if (isset($item) && gettype($item) == "object" && get_class($item) == "MongoDB\BSON\UTCDateTime") {
+
+			return $item->toDateTime();
+		}
+
+		return $item;
+	}
+
+	/**
+	 * @param $tags
+	 *
+	 * @return mixed
+	 */
+	function fixBson($tags){
+		foreach ($tags as $item) {
+
+			$attributes = [];
+			foreach ($item->attributes as $key => $attribute){
+				$attributes[$key] = $this->formatTypeBson($attribute);
+			}
+			$item->attributes = $attributes;
+		}
+		return $tags;
+	}
 }
