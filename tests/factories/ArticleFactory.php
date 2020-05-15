83b88c22a81b86300ff47754401eb5eb6f4d3bd9
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2020-05-15 11:15:11 +0200

    [NS] create ArticleFactory

---
+++ tests/factories/ArticleFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use \Illuminate\Database\Eloquent\Factory;
+use Tests\Models\Article;
+use Faker\Generator as Faker;
+
+/** @var Factory $factory */
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(20)
+    ];
+});
