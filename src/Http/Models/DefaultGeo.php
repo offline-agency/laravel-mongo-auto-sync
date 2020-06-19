1b3d2dd0121e8278561d51ec365bd8b4359173de
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2020-06-19 18:39:34 +0200

    [GF] Removed wrong import class

--- src/Http/Models/DefaultGeo.php
+++ src/Http/Models/DefaultGeo.php
@@ -2,8 +2,6 @@
 
 namespace OfflineAgency\MongoAutoSync\Http\Models;
 
-use Package\OfflineAgency\MongoAutoSync\Http\Models\MDModel;
-
 class DefaultGeo extends MDModel
 {
     protected $primaryKey = 'cadastralCode';
