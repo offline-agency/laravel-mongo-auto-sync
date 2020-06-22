6ab3626f8498eaf59b07205dea8a4ef3495878e9
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2020-06-22 12:26:08 +0200

    [GF] Added docker and php unit config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use Jenssegers\Mongodb\MongodbQueueServiceProvider;
+use Jenssegers\Mongodb\MongodbServiceProvider;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use OfflineAgency\MongoAutoSync\MongoAutoSyncServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
@@ -18,13 +20,38 @@ abstract class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [
+            MongodbServiceProvider::class,
+            MongodbQueueServiceProvider::class,
             MongoAutoSyncServiceProvider::class
         ];
     }
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('laravel-mongo-auto-sync.model_path', './tests/Models');
+        $app['config']->set('laravel-mongo-auto-sync.model_path', 'tests/Models');
         $app['config']->set('laravel-mongo-auto-sync.model_namespace', 'Tests\Models');
+
+        $config = require 'config/database.php';
+
+        $app['config']->set('app.key', 'ZsZewWyUJ5FsKp9lMwv4tYbNlegQilM7');
+
+        $app['config']->set('database.default', 'mongodb');
+        $app['config']->set('database.connections.mongodb', $config['connections']['mongodb']);
+        $app['config']->set('database.connections.mongodb2', $config['connections']['mongodb']);
+        $app['config']->set('database.connections.dsn_mongodb', $config['connections']['dsn_mongodb']);
+        $app['config']->set('database.connections.dsn_mongodb_db', $config['connections']['dsn_mongodb_db']);
+
+        $app['config']->set('auth.model', 'User');
+        $app['config']->set('auth.providers.users.model', 'User');
+        $app['config']->set('cache.driver', 'array');
+
+        $app['config']->set('queue.default', 'database');
+        $app['config']->set('queue.connections.database', [
+            'driver' => 'mongodb',
+            'table' => 'jobs',
+            'queue' => 'default',
+            'expire' => 60,
+        ]);
+        $app['config']->set('queue.failed.database', 'mongodb2');
     }
 }
