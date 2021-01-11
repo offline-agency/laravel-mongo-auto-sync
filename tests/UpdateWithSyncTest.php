219c9e50b26f6bb1b5a38244c67a7a65a6506a2f
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2021-01-11 23:14:56 +0100

    feat: add more tests

--- tests/UpdateWithSyncTest.php
+++ tests/UpdateWithSyncTest.php
@@ -186,11 +186,60 @@ class UpdateWithSyncTest extends SyncTestCase
 
     public function test_update_with_partial_request_plain_field()
     {
+        //Navigation
+       $this->partialUpdateNavigation();
+
+       //SubItems
+        $this->partialUpdateSubItems();
+    }
+
+    public function test_update_with_partial_request_relationship_field()
+    {
+        //Navigation
+        $this->partialUpdateNavigationRelationship();
+
+        //SubItem
+        //$this->partialUpdateSubItemsRelationship();
+    }
+
+
+    private function partialUpdateNavigation(){
+        //Create a navigation and associated to the sub item on creation
+        $navigation_original = $this->createNavigation();
+
+        $data = [
+            'text' => 'updated',
+            'title' => 'updated',
+        ];
+
+        $options = [
+            'request_type' => 'partial',
+        ];
+        $request = new Request;
+        $navigation_new = $navigation_original->updateWithSync($request, $data, $options);
+
+        //text has been updated?
+        $this->assertEquals('updated', $navigation_new->text);
+        $this->assertEquals('updated', $navigation_new->title[cl()]);
+
+        //all the other fields has not been updated?
+        $this->assertEquals($navigation_original->code, $navigation_new->code);
+        $this->assertEquals($navigation_original->href, $navigation_new->href);
+        $this->assertEquals($navigation_original->date, $navigation_new->date);
+        $this->assertEquals($navigation_original->target, $navigation_new->target);
+        $this->assertEquals($navigation_original->sub_items, $navigation_new->sub_items);
+
+        //clean data
+        $navigation_new->delete();
+    }
+
+    private function partialUpdateSubItems(){
         //Create a navigation and associated to the sub item on creation
         $sub_item_original = $this->createSubItems();
 
         $data = [
             'text' => 'updated',
+            'code' => 'updated',
         ];
 
         $options = [
@@ -201,20 +250,68 @@ class UpdateWithSyncTest extends SyncTestCase
 
         //text has been updated?
         $this->assertEquals('updated', $sub_item_new->text[cl()]);
+        $this->assertEquals('updated', $sub_item_new->code);
 
         //all the other fields has not been updated?
         $this->assertEquals($sub_item_original->navigation->getAttributes(), $sub_item_new->navigation->getAttributes());
-        $this->assertEquals($sub_item_original->code, $sub_item_new->code);
         $this->assertEquals($sub_item_original->href, $sub_item_new->href);
+
+        //clean data
+        $sub_item_new->delete();
     }
 
-    public function test_update_with_partial_request_relationship_field()
-    {
-        //Create a navigation and associated to the sub item on creation
+    private function partialUpdateNavigationRelationship(){
+        //Create a sub_items and associated to the navigation on update
+        $navigation_original = $this->createNavigation();
+        $sub_item_original = $this->createSubItems();
+
+        //Test Update from SubItem
+        $mini_sub_items = $this->getMiniSubItem($sub_item_original->id);
+        $data = [
+            'sub_items' => $mini_sub_items,
+        ];
+
+        $options = [
+            'request_type' => 'partial',
+        ];
+        $request = new Request;
+        $navigation_updated = $navigation_original->updateWithSync($request, $data, $options);
+        $mini_sub_item_updated = $navigation_updated->sub_items[0];
+        $mini_sub_item_original = json_decode($mini_sub_items)[0];
+
+        //navigation has been updated?
+        $this->assertNotEmpty($navigation_updated->sub_items);
+        $this->assertNotNull($mini_sub_item_updated);
+
+        $this->assertEquals($mini_sub_item_original->ref_id, $mini_sub_item_updated->ref_id);
+        $this->assertEquals($mini_sub_item_original->text, $mini_sub_item_updated->text[cl()]);
+        $this->assertEquals($mini_sub_item_original->code, $mini_sub_item_updated->code);
+        $this->assertEquals($mini_sub_item_original->href, $mini_sub_item_updated->href);
+
+        //all the other fields has not been updated?
+        $this->assertEquals($navigation_original->title[cl()], $navigation_updated->title[cl()]);
+        $this->assertEquals($navigation_original->code, $navigation_updated->code);
+        $this->assertEquals($navigation_original->href, $navigation_updated->href);
+        $this->assertEquals($navigation_original->title, $navigation_updated->title);
+        $this->assertEquals($navigation_original->date, $navigation_updated->date);
+        $this->assertEquals($navigation_original->target, $navigation_updated->target);
+
+        //check target - subitems
+
+
+
+        $sub_item_original->delete();
+        $navigation_original->delete();
+    }
+
+    private function partialUpdateSubItemsRelationship(){
+        //Create a navigation and associated to the sub item on update
         $sub_item_original = $this->createSubItems();
         $navigation = $this->createNavigation();
-        $mini_navigation = $this->getMiniNavigation($navigation->id);
 
+
+        //Test Update from SubItem
+        $mini_navigation = $this->getMiniNavigation($navigation->id);
         $data = [
             'navigation' => $mini_navigation,
         ];
@@ -237,5 +334,8 @@ class UpdateWithSyncTest extends SyncTestCase
         $this->assertEquals($sub_item_original->text[cl()], $sub_item_new->text[cl()]);
         $this->assertEquals($sub_item_original->code, $sub_item_new->code);
         $this->assertEquals($sub_item_original->href, $sub_item_new->href);
+
+        $sub_item_original->delete();
+        $navigation->delete();
     }
 }
