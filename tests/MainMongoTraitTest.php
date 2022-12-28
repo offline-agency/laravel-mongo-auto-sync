3ced8a5ae7f638c1ea8434d6803f663623eb2d87
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2022-12-28 17:34:10 +0100

    feat: add main mongo trait tests

---
+++ tests/MainMongoTraitTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests;
+
+use Exception;
+use Illuminate\Http\Request;
+use OfflineAgency\MongoAutoSync\Traits\MainMongoTrait;
+
+class MainMongoTraitTest extends SyncTestCase
+{
+    use MainMongoTrait;
+
+    public function test_checkPropertyExistence()
+    {
+        $this->expectException(Exception::class);
+
+        $this->checkPropertyExistence(
+            (object)['key' => 'value'],
+            'fake_key'
+        );
+    }
+
+    public function test_checkArrayExistence()
+    {
+        $this->expectException(Exception::class);
+
+        $this->checkArrayExistence(
+            ['key' => 'value'],
+            'fake_key'
+        );
+    }
+
+    public function test_checkRequestExistence()
+    {
+        $request = new Request();
+
+        $this->expectException(Exception::class);
+
+        $request = $request->replace([
+            ['key' => 'value'],
+        ]);
+
+        $this->checkRequestExistence(
+            $request,
+            'fake_key'
+        );
+    }
+}
